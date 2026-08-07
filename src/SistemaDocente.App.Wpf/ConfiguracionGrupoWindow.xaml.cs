3ff68a5c3df3042b155b672ba60eaa453d631b5a
Programisto <otilio.latam@gmail.com>
2026-08-07 08:36:50 -0600

    fix(wpf): evita cierre duplicado de configuración

--- src/SistemaDocente.App.Wpf/ConfiguracionGrupoWindow.xaml.cs
+++ src/SistemaDocente.App.Wpf/ConfiguracionGrupoWindow.xaml.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using System.Windows;
 
 using SistemaDocente.Presentation;
@@ -12,7 +11,6 @@ public partial class ConfiguracionGrupoWindow : Window
         ArgumentNullException.ThrowIfNull(viewModel);
         InitializeComponent();
         DataContext = viewModel;
-        viewModel.PropertyChanged += OnViewModelPropertyChanged;
     }
 
     private ConfiguracionGrupoViewModel ViewModel => (ConfiguracionGrupoViewModel)DataContext;
@@ -27,29 +25,11 @@ public partial class ConfiguracionGrupoWindow : Window
         if (ViewModel.GuardadoCorrectamente)
         {
             DialogResult = true;
-            Close();
         }
     }
 
     private void OnCancelarClic(object sender, RoutedEventArgs e)
     {
         DialogResult = false;
-        Close();
-    }
-
-    private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName == nameof(ConfiguracionGrupoViewModel.GuardadoCorrectamente)
-            && ViewModel.GuardadoCorrectamente)
-        {
-            DialogResult = true;
-            Close();
-        }
-    }
-
-    protected override void OnClosed(EventArgs e)
-    {
-        ViewModel.PropertyChanged -= OnViewModelPropertyChanged;
-        base.OnClosed(e);
     }
 }
