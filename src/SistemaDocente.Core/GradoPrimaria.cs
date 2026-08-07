7945926f554cbe33a5a24c5eaf7fee92032b9b63
Programisto <otilio.latam@gmail.com>
2026-08-07 12:40:28 -0600

    feat: add structured primary grade model

---
+++ src/SistemaDocente.Core/GradoPrimaria.cs
@@ -0,0 +1,12 @@
+namespace SistemaDocente.Core;
+
+public enum GradoPrimaria
+{
+    NoEspecificado = 0,
+    Primero = 1,
+    Segundo = 2,
+    Tercero = 3,
+    Cuarto = 4,
+    Quinto = 5,
+    Sexto = 6,
+}
