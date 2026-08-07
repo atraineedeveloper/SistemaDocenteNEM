e10bc1780f566601aaee5f65a6a8567f1bff05cf
Programisto <otilio.latam@gmail.com>
2026-08-07 13:37:57 -0600

    feat: add NEM project methodology catalog

---
+++ src/SistemaDocente.Core/MetodologiaProyectoNem.cs
@@ -0,0 +1,10 @@
+namespace SistemaDocente.Core;
+
+public enum MetodologiaProyectoNem
+{
+    NoEspecificada = 0,
+    ProyectosComunitarios = 1,
+    IndagacionSteam = 2,
+    AprendizajeBasadoEnProblemas = 3,
+    AprendizajeServicio = 4,
+}
